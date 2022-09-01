fe38d5ea689835e4165fc3f25119d4fc1a7b7ee8
Nestor Quiroga <nqs48@icloud.com>
2022-09-01 09:49:54 -0500

    Creating winner atributte <Ganador>

--- domain/src/main/java/com/sofka/domain/Juego.java
+++ domain/src/main/java/com/sofka/domain/Juego.java
@@ -5,15 +5,16 @@ import com.sofka.domain.entities.Jugador;
 import com.sofka.domain.entities.Tablero;
 import com.sofka.domain.values.Estado;
 import com.sofka.domain.values.JuegoId;
+import com.sofka.domain.values.JugadorId;
+
+import java.util.Map;
 
 public class Juego extends AggregateEvent<JuegoId> {
 
     protected Tablero tablero;
-    protected Set<Jugador> jugadores;
+    protected Map<JugadorId, Jugador> jugadores;
     protected Estado estado;
     protected Jugador ganador;
-
-
 
 
 
