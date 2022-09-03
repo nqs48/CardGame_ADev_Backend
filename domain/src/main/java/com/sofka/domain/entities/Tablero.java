2e86b049bed999b3b69952ca636e370fecaf76e0
Nestor Quiroga <nqs48@icloud.com>
2022-09-02 21:04:18 -0500

    Updating Entity <Tablero> with properties and constructor

--- domain/src/main/java/com/sofka/domain/entities/Tablero.java
+++ domain/src/main/java/com/sofka/domain/entities/Tablero.java
@@ -1,13 +1,29 @@
 package com.sofka.domain.entities;
 
 import co.com.sofka.domain.generic.Entity;
+import com.sofka.domain.values.Carta;
+import com.sofka.domain.values.JugadorId;
 import com.sofka.domain.values.TableroId;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Tablero extends Entity<TableroId> {
 
+    private Integer tiempo;
+
+    private Boolean estaHabilitado;
+    private final Map<JugadorId, Set<Carta>> partida;
 
 
-
+    public Tablero(TableroId tableroId, Set<JugadorId> jugadoresId) {
+        super(tableroId);
+        this.estaHabilitado = false;
+        this.partida = new HashMap<>();
+        jugadoresId.forEach(jugadorId -> partida.put(jugadorId, new HashSet<>()));
+    }
 
 
 }
