8b615d3d2a2542f65954555b678bb8d6b9d3dbd2
Nestor Quiroga <nqs48@icloud.com>
2022-09-03 13:18:02 -0500

    Creating Command <IniciarJuego>

---
+++ domain/src/main/java/com/sofka/cardgame/commands/IniciarJuegoCommand.java
@@ -0,0 +1,14 @@
+package com.sofka.cardgame.commands;
+
+public class IniciarJuegoCommand {
+
+    private String juegoId;
+
+    public String getJuegoId() {
+        return juegoId;
+    }
+
+    public void setJuegoId(String juegoId) {
+        this.juegoId = juegoId;
+    }
+}
