980d74fd7408cad94fc3947ae687897a403051b9
Nestor Quiroga <nqs48@icloud.com>
2022-09-11 14:00:09 -0500

    Updating Usecase and test for <IniciarJuego>

--- business/src/test/java/com/sofka/cardgame/usecases/IniciarJuegoUseCaseTest.java
+++ business/src/test/java/com/sofka/cardgame/usecases/IniciarJuegoUseCaseTest.java
@@ -1,7 +1,61 @@
 package com.sofka.cardgame.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.cardgame.commands.CrearRondaCommand;
+import com.sofka.cardgame.commands.IniciarJuegoCommand;
+import com.sofka.cardgame.events.JuegoCreado;
+import com.sofka.cardgame.events.TableroCreado;
+import com.sofka.cardgame.gateway.JuegoDomainEventRepository;
+import com.sofka.cardgame.values.JuegoId;
+import com.sofka.cardgame.values.JugadorId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class IniciarJuegoUseCaseTest {
+
+    @InjectMocks
+    private IniciarJuegoUseCase useCase;
+
+    @Mock
+    private JuegoDomainEventRepository repository;
+
+
+    @Test
+    void iniciarJuegoHappyPass() {
+
+        //Arrange
+        var juegoId = JuegoId.of("JuegoId-001");
+        var comando = new IniciarJuegoCommand(juegoId.value());
+
+        when(repository.obtenerEventosPor(juegoId.value())).thenReturn(eventos());
+
+        StepVerifier.create(useCase.apply(Mono.just(comando)))
+                .expectNextMatches(eventoDominio->{
+                    var evento = (TableroCreado) eventoDominio;
+                    return "JuegoId-001".equals(evento.aggregateRootId());
+                })
+                .expectComplete()
+                .verify();
+
+
+    }
+
+    private Flux<DomainEvent> eventos() {
+        return Flux.just(
+                new JuegoCreado(JugadorId.of("Jugador01")));
+    }
+
 
 }
--- domain/src/main/java/com/sofka/cardgame/commands/IniciarJuegoCommand.java
+++ domain/src/main/java/com/sofka/cardgame/commands/IniciarJuegoCommand.java
@@ -5,6 +5,10 @@ import co.com.sofka.domain.generic.Command;
 public class IniciarJuegoCommand extends Command {
     private String juegoId;
 
+    public IniciarJuegoCommand(String juegoId) {
+        this.juegoId = juegoId;
+    }
+
     public String getJuegoId() {
         return juegoId;
     }
