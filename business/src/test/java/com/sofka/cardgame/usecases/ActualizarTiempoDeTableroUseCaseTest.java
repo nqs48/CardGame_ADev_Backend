495438f9a816ad3c69eaf79345cefe66db802083
Nestor Quiroga <nqs48@icloud.com>
2022-09-11 13:28:45 -0500

    Updating Usecase for <ActualizarTiempoDelTablero>

--- business/src/test/java/com/sofka/cardgame/usecases/ActualizarTiempoDeTableroUseCaseTest.java
+++ business/src/test/java/com/sofka/cardgame/usecases/ActualizarTiempoDeTableroUseCaseTest.java
@@ -1,7 +1,11 @@
 package com.sofka.cardgame.usecases;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ActualizarTiempoDeTableroUseCaseTest {
 
 }
