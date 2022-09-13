5ee0b76e3d0871d03233a3001a3075e8579caa23
Nestor Quiroga <nqs48@icloud.com>
2022-09-13 06:35:42 -0500

    Adding commands for <IniciarRonda>,<PonerCarta>,<IniciarJuego>

--- application/app-service-two/src/main/java/com/sofka/cardgame/application/handle/CommandHandle.java
+++ application/app-service-two/src/main/java/com/sofka/cardgame/application/handle/CommandHandle.java
@@ -1,7 +1,13 @@
 package com.sofka.cardgame.application.handle;
 
 import com.sofka.cardgame.commands.CrearJuegoCommand;
+import com.sofka.cardgame.commands.IniciarJuegoCommand;
+import com.sofka.cardgame.commands.IniciarRondaCommand;
+import com.sofka.cardgame.commands.PonerCartaEnTableroCommand;
 import com.sofka.cardgame.usecases.CrearJuegoUseCase;
+import com.sofka.cardgame.usecases.IniciarJuegoUseCase;
+import com.sofka.cardgame.usecases.IniciarRondaUseCase;
+import com.sofka.cardgame.usecases.PonerCartaEnTableroUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -33,6 +39,41 @@ public class CommandHandle {
                             .apply(request.bodyToMono(CrearJuegoCommand.class))
                             .then(ServerResponse.ok().build());
                 }
+
+        );
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> iniciar(IniciarJuegoUseCase usecase) {
+        return route(
+                POST("/juego/iniciar").and(accept(MediaType.APPLICATION_JSON)),
+                request -> usecase.andThen(integrationHandle)
+                        .apply(request.bodyToMono(IniciarJuegoCommand.class))
+                        .then(ServerResponse.ok().build())
+
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> poner(PonerCartaEnTableroUseCase usecase) {
+        return route(
+                POST("/juego/poner").and(accept(MediaType.APPLICATION_JSON)),
+                request -> usecase.andThen(integrationHandle)
+                        .apply(request.bodyToMono(PonerCartaEnTableroCommand.class))
+                        .then(ServerResponse.ok().build())
+
+        );
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> iniciarRonda(IniciarRondaUseCase usecase) {
+        return route(
+                POST("/juego/ronda/iniciar").and(accept(MediaType.APPLICATION_JSON)),
+                request -> usecase.andThen(integrationHandle)
+                        .apply(request.bodyToMono(IniciarRondaCommand.class))
+                        .then(ServerResponse.ok().build())
 
         );
     }
